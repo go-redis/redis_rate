aea2c16044f02c73b5f584944f3449c47b099e7b
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2016-09-28 09:32:23 +0000

    Add ResetRate.

--- rate.go
+++ rate.go
@@ -3,13 +3,15 @@ package rate // import "gopkg.in/go-redis/rate.v4"
 import (
 	"fmt"
 	"time"
-	redis "gopkg.in/redis.v4"
+
 	timerate "golang.org/x/time/rate"
+	redis "gopkg.in/redis.v4"
 )
 
 const redisPrefix = "rate"
 
 type rediser interface {
+	Del(...string) *redis.IntCmd
 	Pipelined(func(pipe *redis.Pipeline) error) ([]redis.Cmder, error)
 }
 
@@ -28,15 +30,34 @@ func NewLimiter(redis rediser, fallbackLimiter *timerate.Limiter) *Limiter {
 	}
 }
 
-//Reset will reset the rate limit for a single key.
-//In the given rate limit window.
-func (l *Limiter) Reset(name string, dur time.Duration) (err error) {
+// Reset resets the rate limit for the name in the given rate limit window.
+func (l *Limiter) Reset(name string, dur time.Duration) error {
 	udur := int64(dur / time.Second)
 	slot := time.Now().Unix() / udur
-	name = fmt.Sprintf("%s:%s-%d", redisPrefix, name, slot)
-	err = l.delete(name)
-	
-	return
+
+	name = allowName(name, slot)
+	return l.redis.Del(name).Err()
+}
+
+// Reset resets the rate limit for the name and limit.
+func (l *Limiter) ResetRate(name string, rateLimit timerate.Limit) error {
+	if rateLimit == 0 {
+		return nil
+	}
+	if rateLimit == timerate.Inf {
+		return nil
+	}
+
+	dur := time.Second
+	limit := int64(rateLimit)
+	if limit == 0 {
+		limit = 1
+		dur *= time.Duration(1 / rateLimit)
+	}
+	slot := time.Now().UnixNano() / dur.Nanoseconds()
+
+	name = allowRateName(name, dur, slot)
+	return l.redis.Del(name).Err()
 }
 
 // AllowN reports whether an event with given name may happen at time now.
@@ -47,13 +68,13 @@ func (l *Limiter) AllowN(name string, maxn int64, dur time.Duration, n int64) (c
 	slot := time.Now().Unix() / udur
 	reset = (slot + 1) * udur
 	allow = l.fallbackLimiter.Allow()
-	
-	name = fmt.Sprintf("%s:%s-%d", redisPrefix, name, slot)
+
+	name = allowName(name, slot)
 	count, err := l.incr(name, dur, n)
 	if err == nil {
 		allow = count <= maxn
 	}
-	
+
 	return count, reset, allow
 }
 
@@ -81,45 +102,29 @@ func (l *Limiter) AllowRate(name string, rateLimit timerate.Limit) (delay time.D
 	if rateLimit == timerate.Inf {
 		return 0, true
 	}
-	
+
 	dur := time.Second
 	limit := int64(rateLimit)
 	if limit == 0 {
 		limit = 1
 		dur *= time.Duration(1 / rateLimit)
 	}
-	
 	now := time.Now()
 	slot := now.UnixNano() / dur.Nanoseconds()
+
 	allow = l.fallbackLimiter.Allow()
-	
-	name = fmt.Sprintf("%s:%s-%d-%d", redisPrefix, name, dur, slot)
+
+	name = allowRateName(name, dur, slot)
 	count, err := l.incr(name, dur, 1)
 	if err == nil {
 		allow = count <= limit
 	}
-	
+
 	if !allow {
 		delay = time.Duration(slot+1)*dur - time.Duration(now.UnixNano())
 	}
-	
-	return delay, allow
-}
 
-func (l *Limiter) delete(name string) (err error) {
-	var del *redis.IntCmd
-	_, err = l.redis.Pipelined(func(pipe *redis.Pipeline) error {
-		del = pipe.Del(name)
-		return nil
-	})
-	
-	if err != nil {
-		return
-	}
-	
-	err = del.Err()
-	
-	return
+	return delay, allow
 }
 
 func (l *Limiter) incr(name string, dur time.Duration, n int64) (int64, error) {
@@ -129,7 +134,15 @@ func (l *Limiter) incr(name string, dur time.Duration, n int64) (int64, error) {
 		pipe.Expire(name, dur)
 		return nil
 	})
-	
+
 	rate, _ := incr.Result()
 	return rate, err
+}
+
+func allowName(name string, slot int64) string {
+	return fmt.Sprintf("%s:%s-%d", redisPrefix, name, slot)
+}
+
+func allowRateName(name string, dur time.Duration, slot int64) string {
+	return fmt.Sprintf("%s:%s-%d-%d", redisPrefix, name, dur, slot)
 }
