53c8a9b78cc83ef8fb52a1589fb2eaf1944cc333
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2020-11-19 15:26:09 +0200

    Update dependencies

--- rate_test.go
+++ rate_test.go
@@ -6,7 +6,7 @@ import (
 	"time"
 
 	"github.com/go-redis/redis/v8"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 
 	"github.com/go-redis/redis_rate/v9"
 )
@@ -27,46 +27,45 @@ func TestAllow(t *testing.T) {
 	l := rateLimiter()
 
 	limit := redis_rate.PerSecond(10)
-	assert.Equal(t, limit.String(), "10 req/s (burst 10)")
-	assert.False(t, limit.IsZero())
+	require.Equal(t, limit.String(), "10 req/s (burst 10)")
+	require.False(t, limit.IsZero())
 
 	res, err := l.Allow(ctx, "test_id", limit)
-	assert.Nil(t, err)
-	assert.Equal(t, res.Allowed, 1)
-	assert.Equal(t, res.Remaining, 9)
-	assert.Equal(t, res.RetryAfter, time.Duration(-1))
-	assert.InDelta(t, res.ResetAfter, 100*time.Millisecond, float64(10*time.Millisecond))
+	require.Nil(t, err)
+	require.Equal(t, res.Allowed, 1)
+	require.Equal(t, res.Remaining, 9)
+	require.Equal(t, res.RetryAfter, time.Duration(-1))
+	require.InDelta(t, res.ResetAfter, 100*time.Millisecond, float64(10*time.Millisecond))
 
 	err = l.Reset(ctx, "test_id")
-	assert.Nil(t, err)
+	require.Nil(t, err)
 	res, err = l.Allow(ctx, "test_id", limit)
-	assert.Nil(t, err)
-	assert.Equal(t, res.Allowed, 1)
-	assert.Equal(t, res.Remaining, 9)
-	assert.Equal(t, res.RetryAfter, time.Duration(-1))
-	assert.InDelta(t, res.ResetAfter, 100*time.Millisecond, float64(10*time.Millisecond))
+	require.Nil(t, err)
+	require.Equal(t, res.Allowed, 1)
+	require.Equal(t, res.Remaining, 9)
+	require.Equal(t, res.RetryAfter, time.Duration(-1))
+	require.InDelta(t, res.ResetAfter, 100*time.Millisecond, float64(10*time.Millisecond))
 
 	res, err = l.AllowN(ctx, "test_id", limit, 2)
-	assert.Nil(t, err)
-	assert.Equal(t, res.Allowed, 2)
-	assert.Equal(t, res.Remaining, 7)
-	assert.Equal(t, res.RetryAfter, time.Duration(-1))
-	assert.InDelta(t, res.ResetAfter, 300*time.Millisecond, float64(10*time.Millisecond))
+	require.Nil(t, err)
+	require.Equal(t, res.Allowed, 2)
+	require.Equal(t, res.Remaining, 7)
+	require.Equal(t, res.RetryAfter, time.Duration(-1))
+	require.InDelta(t, res.ResetAfter, 300*time.Millisecond, float64(10*time.Millisecond))
 
 	res, err = l.AllowN(ctx, "test_id", limit, 7)
-	assert.Nil(t, err)
-	assert.Equal(t, res.Allowed, 7)
-	assert.Equal(t, res.Remaining, 0)
-	assert.Equal(t, res.RetryAfter, time.Duration(-1))
-	assert.InDelta(t, res.ResetAfter, 999*time.Millisecond, float64(10*time.Millisecond))
+	require.Nil(t, err)
+	require.Equal(t, res.Allowed, 7)
+	require.Equal(t, res.Remaining, 0)
+	require.Equal(t, res.RetryAfter, time.Duration(-1))
+	require.InDelta(t, res.ResetAfter, 999*time.Millisecond, float64(10*time.Millisecond))
 
 	res, err = l.AllowN(ctx, "test_id", limit, 1000)
-	assert.Nil(t, err)
-	assert.Equal(t, res.Allowed, 0)
-	assert.Equal(t, res.Remaining, 0)
-	assert.InDelta(t, res.RetryAfter, 99*time.Second, float64(time.Second))
-	assert.InDelta(t, res.ResetAfter, 999*time.Millisecond, float64(10*time.Millisecond))
-
+	require.Nil(t, err)
+	require.Equal(t, res.Allowed, 0)
+	require.Equal(t, res.Remaining, 0)
+	require.InDelta(t, res.RetryAfter, 99*time.Second, float64(time.Second))
+	require.InDelta(t, res.ResetAfter, 999*time.Millisecond, float64(10*time.Millisecond))
 }
 
 func TestRetryAfter(t *testing.T) {
@@ -81,13 +80,13 @@ func TestRetryAfter(t *testing.T) {
 
 	for i := 0; i < 1000; i++ {
 		res, err := l.Allow(ctx, "test_id", limit)
-		assert.Nil(t, err)
+		require.Nil(t, err)
 
 		if res.Allowed > 0 {
 			continue
 		}
 
-		assert.LessOrEqual(t, int64(res.RetryAfter), int64(time.Millisecond))
+		require.LessOrEqual(t, int64(res.RetryAfter), int64(time.Millisecond))
 	}
 }
 
@@ -98,53 +97,53 @@ func TestAllowAtMost(t *testing.T) {
 	limit := redis_rate.PerSecond(10)
 
 	res, err := l.Allow(ctx, "test_id", limit)
-	assert.Nil(t, err)
-	assert.Equal(t, res.Allowed, 1)
-	assert.Equal(t, res.Remaining, 9)
-	assert.Equal(t, res.RetryAfter, time.Duration(-1))
-	assert.InDelta(t, res.ResetAfter, 100*time.Millisecond, float64(10*time.Millisecond))
+	require.Nil(t, err)
+	require.Equal(t, res.Allowed, 1)
+	require.Equal(t, res.Remaining, 9)
+	require.Equal(t, res.RetryAfter, time.Duration(-1))
+	require.InDelta(t, res.ResetAfter, 100*time.Millisecond, float64(10*time.Millisecond))
 
 	res, err = l.AllowAtMost(ctx, "test_id", limit, 2)
-	assert.Nil(t, err)
-	assert.Equal(t, res.Allowed, 2)
-	assert.Equal(t, res.Remaining, 7)
-	assert.Equal(t, res.RetryAfter, time.Duration(-1))
-	assert.InDelta(t, res.ResetAfter, 300*time.Millisecond, float64(10*time.Millisecond))
+	require.Nil(t, err)
+	require.Equal(t, res.Allowed, 2)
+	require.Equal(t, res.Remaining, 7)
+	require.Equal(t, res.RetryAfter, time.Duration(-1))
+	require.InDelta(t, res.ResetAfter, 300*time.Millisecond, float64(10*time.Millisecond))
 
 	res, err = l.AllowN(ctx, "test_id", limit, 0)
-	assert.Nil(t, err)
-	assert.Equal(t, res.Allowed, 0)
-	assert.Equal(t, res.Remaining, 7)
-	assert.Equal(t, res.RetryAfter, time.Duration(-1))
-	assert.InDelta(t, res.ResetAfter, 300*time.Millisecond, float64(10*time.Millisecond))
+	require.Nil(t, err)
+	require.Equal(t, res.Allowed, 0)
+	require.Equal(t, res.Remaining, 7)
+	require.Equal(t, res.RetryAfter, time.Duration(-1))
+	require.InDelta(t, res.ResetAfter, 300*time.Millisecond, float64(10*time.Millisecond))
 
 	res, err = l.AllowAtMost(ctx, "test_id", limit, 10)
-	assert.Nil(t, err)
-	assert.Equal(t, res.Allowed, 7)
-	assert.Equal(t, res.Remaining, 0)
-	assert.Equal(t, res.RetryAfter, time.Duration(-1))
-	assert.InDelta(t, res.ResetAfter, 999*time.Millisecond, float64(10*time.Millisecond))
+	require.Nil(t, err)
+	require.Equal(t, res.Allowed, 7)
+	require.Equal(t, res.Remaining, 0)
+	require.Equal(t, res.RetryAfter, time.Duration(-1))
+	require.InDelta(t, res.ResetAfter, 999*time.Millisecond, float64(10*time.Millisecond))
 
 	res, err = l.AllowN(ctx, "test_id", limit, 0)
-	assert.Nil(t, err)
-	assert.Equal(t, res.Allowed, 0)
-	assert.Equal(t, res.Remaining, 0)
-	assert.Equal(t, res.RetryAfter, time.Duration(-1))
-	assert.InDelta(t, res.ResetAfter, 999*time.Millisecond, float64(10*time.Millisecond))
+	require.Nil(t, err)
+	require.Equal(t, res.Allowed, 0)
+	require.Equal(t, res.Remaining, 0)
+	require.Equal(t, res.RetryAfter, time.Duration(-1))
+	require.InDelta(t, res.ResetAfter, 999*time.Millisecond, float64(10*time.Millisecond))
 
 	res, err = l.AllowAtMost(ctx, "test_id", limit, 1000)
-	assert.Nil(t, err)
-	assert.Equal(t, res.Allowed, 0)
-	assert.Equal(t, res.Remaining, 0)
-	assert.InDelta(t, res.RetryAfter, 99*time.Millisecond, float64(10*time.Millisecond))
-	assert.InDelta(t, res.ResetAfter, 999*time.Millisecond, float64(10*time.Millisecond))
+	require.Nil(t, err)
+	require.Equal(t, res.Allowed, 0)
+	require.Equal(t, res.Remaining, 0)
+	require.InDelta(t, res.RetryAfter, 99*time.Millisecond, float64(10*time.Millisecond))
+	require.InDelta(t, res.ResetAfter, 999*time.Millisecond, float64(10*time.Millisecond))
 
 	res, err = l.AllowN(ctx, "test_id", limit, 1000)
-	assert.Nil(t, err)
-	assert.Equal(t, res.Allowed, 0)
-	assert.Equal(t, res.Remaining, 0)
-	assert.InDelta(t, res.RetryAfter, 99*time.Second, float64(time.Second))
-	assert.InDelta(t, res.ResetAfter, 999*time.Millisecond, float64(10*time.Millisecond))
+	require.Nil(t, err)
+	require.Equal(t, res.Allowed, 0)
+	require.Equal(t, res.Remaining, 0)
+	require.InDelta(t, res.RetryAfter, 99*time.Second, float64(time.Second))
+	require.InDelta(t, res.ResetAfter, 999*time.Millisecond, float64(10*time.Millisecond))
 }
 
 func BenchmarkAllow(b *testing.B) {
